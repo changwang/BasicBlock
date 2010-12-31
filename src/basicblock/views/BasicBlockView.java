f4e5f5853963c7c2b81ac481db8e58135e80094f
Chang Wang <changwang83@gmail.com>
2010-12-31 13:07:05 -0500

    use listener service to reflect selected source

--- src/basicblock/views/BasicBlockView.java
+++ src/basicblock/views/BasicBlockView.java
@@ -1,21 +1,21 @@
 package basicblock.views;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.zest.core.viewers.AbstractZoomableViewer;
 import org.eclipse.zest.core.viewers.GraphViewer;
@@ -23,6 +23,7 @@ import org.eclipse.zest.core.viewers.IEntityConnectionStyleProvider;
 import org.eclipse.zest.core.viewers.IGraphContentProvider;
 import org.eclipse.zest.core.viewers.IZoomableWorkbenchPart;
 import org.eclipse.zest.core.viewers.ZoomContributionViewItem;
+import org.eclipse.zest.core.widgets.GraphNode;
 import org.eclipse.zest.core.widgets.ZestStyles;
 import org.eclipse.zest.layouts.LayoutStyles;
 import org.eclipse.zest.layouts.algorithms.TreeLayoutAlgorithm;
@@ -36,24 +37,64 @@ public class BasicBlockView extends ViewPart implements IZoomableWorkbenchPart {
 	public static final String ID = "BasicBlock.basicblockview";
 
 	private GraphViewer gViewer = null;
+	private GraphNode errorNode = null;
+	private List<BasicBlock> blocks = null;
 
 	public BasicBlockView() {
 	}
 
+	private ISelectionListener listener = new ISelectionListener() {
+
+		@Override
+		public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+			if (part != BasicBlockView.this
+					&& selection instanceof ITextSelection) {
+				ITextSelection ts = (ITextSelection) selection;
+				if (ts.getText().length() > 0) {
+					blocks = basicBlocksFromSource(ts.getText());
+					if (blocks.size() == 1) {
+						BasicBlock bb = blocks.get(0);
+						if (bb.getContent().size() < 1) {
+							if (errorNode != null)
+								errorNode.dispose();
+							errorNode = new GraphNode(
+									gViewer.getGraphControl(), SWT.NONE);
+							errorNode
+									.setText("Selected source code cannot be parsed");
+						} else {
+							drawHierarchy(blocks);
+						}
+					} else {
+						drawHierarchy(blocks);
+					}
+				}
+			}
+		}
+	};
+
 	@Override
 	public void createPartControl(Composite parent) {
-		List<BasicBlock> blocks = basicBlocksFromSource();
 
 		gViewer = new GraphViewer(parent, SWT.BORDER);
+
 		gViewer.setContentProvider(new BasicBlockContentProvider());
 		gViewer.setLabelProvider(new BasicBlockLabelProvider());
-		BasicBlockConnectionRelationshipProvider bbncp = new BasicBlockConnectionRelationshipProvider(
-				blocks);
-		gViewer.setInput(bbncp.getConnections());
 		gViewer.setLayoutAlgorithm(new TreeLayoutAlgorithm(
 				LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
 		gViewer.applyLayout();
+
 		fillToolBar();
+
+		getSite().getWorkbenchWindow().getSelectionService()
+				.addPostSelectionListener(listener);
+	}
+
+	private void drawHierarchy(List<BasicBlock> blocks) {
+		if (errorNode != null)
+			errorNode.dispose();
+		BasicBlockConnectionRelationshipProvider bbncp = new BasicBlockConnectionRelationshipProvider(
+				blocks);
+		gViewer.setInput(bbncp.getConnections());
 	}
 
 	private void fillToolBar() {
@@ -67,8 +108,7 @@ public class BasicBlockView extends ViewPart implements IZoomableWorkbenchPart {
 	public void setFocus() {
 	}
 
-	private List<BasicBlock> basicBlocksFromSource() {
-		String source = sourceFromFile("ifelseblock3");
+	private List<BasicBlock> basicBlocksFromSource(String source) {
 		ASTNode root = (Statement) Helper.getParser(source).createAST(null);
 		BasicBlockVisitor bbv = new BasicBlockVisitor();
 		bbv.setRoot(root);
@@ -76,30 +116,16 @@ public class BasicBlockView extends ViewPart implements IZoomableWorkbenchPart {
 		return bbv.getBlocks();
 	}
 
-	private String sourceFromFile(String filename) {
-		BufferedReader br;
-		StringBuilder sb = new StringBuilder();
-		try {
-			br = new BufferedReader(new FileReader(
-					"/Users/chang/eclipse/workspace/BasicBlock/src/fixtures/"
-							+ filename));
-
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				sb.append(line);
-			}
-			br.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return sb.toString();
-	}
-
 	@Override
 	public AbstractZoomableViewer getZoomableViewer() {
 		return gViewer;
+	}
+
+	@Override
+	public void dispose() {
+		getSite().getWorkbenchWindow().getSelectionService()
+				.removePostSelectionListener(listener);
+		super.dispose();
 	}
 
 }
@@ -142,10 +168,8 @@ class BasicBlockConnectionRelationshipProvider {
 
 	public BasicBlockConnectionRelationshipProvider(List<BasicBlock> nodes) {
 		connections = new ArrayList<BBConnection>();
-		// this.nodes = nodes;
 
 		// create all relationships between all basic blocks
-
 		if (nodes.size() == 1) {
 			connections.add(new BBConnection("", nodes.get(0), null));
 		} else if (nodes.size() > 1) {
@@ -167,10 +191,6 @@ class BasicBlockConnectionRelationshipProvider {
 	public List<BBConnection> getConnections() {
 		return connections;
 	}
-
-	// public List<BasicBlock> getNodes() {
-	// return nodes;
-	// }
 }
 
 class BasicBlockContentProvider extends ArrayContentProvider implements
@@ -228,6 +248,10 @@ class BasicBlockLabelProvider extends LabelProvider implements
 
 	@Override
 	public IFigure getTooltip(Object entity) {
+		System.out.println(entity);
+		if (entity instanceof BasicBlock) {
+			return new ContentFigure(((BasicBlock) entity).getContent());
+		}
 		return null;
 	}
 }
